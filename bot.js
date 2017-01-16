a0c9302dad079bf0f30df5e598ef8fa5f2fa6d1c
Madison Grams <mgrams3@gatech.edu>
2017-01-16 15:38:48 -0500

    trying out something

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /meme/;
+      botRegex = /coming/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -22,7 +22,7 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   //botResponse = cool();
-  botResponse = "hey";
+  botResponse = "arriving";
 
   options = {
     hostname: 'api.groupme.com',
