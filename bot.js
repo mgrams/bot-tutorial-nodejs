b59f832706f46f39076eefb2d92343e1b0df70df
Madison Grams <mgrams3@gatech.edu>
2017-01-16 15:35:14 -0500

    trying out something

--- bot.js
+++ bot.js
@@ -21,7 +21,8 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  //botResponse = cool();
+  botResponse = "hey";
 
   options = {
     hostname: 'api.groupme.com',
